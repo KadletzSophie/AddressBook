7d524a7413d3e74980acd28ac1dc86a1d060e5d8
Sophie Kadletz <sophie.kadletz@htl-wels.at>
2021-02-18 20:34:04 +0100

    GUI implemented

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,25 @@
-package sample;
-
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * @author Sophie Kadletz
+ * @version 18.02.2021
+ */
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+    public void start(Stage primaryStage) throws Exception
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("view/sample.fxml"));
+        primaryStage.setTitle("Address Book");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
-    }
 
+    }
 
     public static void main(String[] args) {
         launch(args);
---
+++ src/controller/Controller.java
@@ -0,0 +1,84 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class Controller {
+
+    @FXML
+    private TextField name_txt;
+
+    @FXML
+    private TextField address_txt;
+
+    @FXML
+    private TextField phone_txt;
+
+    @FXML
+    private Button pref_bt;
+
+    @FXML
+    private Button loadCSV_bt;
+
+    @FXML
+    private Button deleteEntry;
+
+    @FXML
+    private Button saveChanges_bt;
+
+    @FXML
+    private Button next_bt;
+
+    @FXML
+    private Button saveCSV_bt;
+
+    @FXML
+    private Label page_lbl;
+
+    @FXML
+    private Button searchEntry;
+
+    @FXML
+    void AddEntry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void DeleteEntry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void LoadFromCSV(ActionEvent event) {
+
+    }
+
+    @FXML
+    void NextEntry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void PrefEntry(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveChanges(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SaveToCSV(ActionEvent event) {
+
+    }
+
+    @FXML
+    void searchEntry(ActionEvent event) {
+
+    }
+
+}
